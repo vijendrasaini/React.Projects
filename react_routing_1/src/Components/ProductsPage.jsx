15345e558b55da97476ab00531caecba599745db
Vijendra <docs.vijendra@gmail.com>
2022-03-17 05:52:00 +0530

    page to show all products is added.

---
+++ react_routing_1/src/Components/ProductsPage.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export const ProductsPage = () => {
+
+  const [ products , setProducts] = useState([])
+  useEffect(()=>{
+    getProducts()
+  }, [])
+  async function getProducts(){
+    const a = await fetch('http://localhost:7000/products')
+    const b = await a.json()
+    setProducts(b)
+  }
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 35%)",
+          justifyContent: "space-evenly",
+          width: "500px",
+          margin: "auto",
+          paddingTop: "50px",
+        }}
+      >
+        <h4>Product Name</h4>
+        <h4>Price</h4>
+        <h4>Details</h4>
+      </div>
+      {products.map((el) => {
+        return (
+          <div
+            className="container"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(4, 35%)",
+              justifyContent: "space-evenly",
+              width: "500px",
+              margin: "auto",
+              paddingTop: "10px",
+            }}
+          >
+            <div className="name">{el.title}</div>
+            <div className="price">{el.price}</div>
+            <Link to={`/products/${el.id}`}>more details</Link>
+          </div>
+        );
+      })}
+    </>
+  );
+};
