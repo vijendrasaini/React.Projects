0e300c5325b63a8780266b86a99f7d8525face7e
Vijendra <docs.vijendra@gmail.com>
2022-03-17 05:51:30 +0530

    404 error page  is added

---
+++ react_routing_1/src/Components/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export const NotFoundPage = () => {
+  return (
+    <div className="notfound">
+      <img src="https://cdn4.vectorstock.com/i/1000x1000/85/43/error-page-not-found-vector-27898543.jpg" alt="" />
+    </div>
+  );
+};
